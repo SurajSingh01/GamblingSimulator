a06e06c368e780dfb53daae030b293cdea37c0a8
SurajSingh01 <singhsurajd03@gmail.com>
2020-07-30 16:00:36 +0530

    Modified for exit for winning or lossing 50% of stake

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -8,13 +8,14 @@ betPerGame=1
 echo "Stakes per day would be \$$stakePerDay"
 echo "Bet to be placed per game will be \$$betPerGame"
 
-flip=$(( RANDOM % 2 ))
-if [ $flip -eq 1 ]
-then
-	echo "You won the bet"
-	((stakePerDay++))
-else
-	echo "You lost the bet"
-	((stakePerDay--))
-fi
+while (( $stakePerDay >50 && $stakePerDay < 150 ))
+do
+	flip=$(( RANDOM % 2 ))
+		if [ $flip -eq 1 ]
+		then
+			((stakePerDay++))
+		else
+			((stakePerDay--))
+		fi
+done
 echo "You are left with \$$stakePerDay"

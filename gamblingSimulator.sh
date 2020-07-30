f577427076d6b1d8b999cdbe635c674eef17dc25
SurajSingh01 <singhsurajd03@gmail.com>
2020-07-30 20:01:17 +0530

    Modified for the calculation of win and loss result for 20 days of gambling

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,21 +1,37 @@
 
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome to gambling Simulator"
 
 #Intialization of variable 
 stakePerDay=100
 betPerGame=1
+daysPlayed=0
+total=0
 echo "Stakes per day would be \$$stakePerDay"
 echo "Bet to be placed per game will be \$$betPerGame"
 
-while (( $stakePerDay >50 && $stakePerDay < 150 ))
+while (( $daysPlayed < 20 ))
 do
-	flip=$(( RANDOM % 2 ))
-		if [ $flip -eq 1 ]
-		then
-			((stakePerDay++))
-		else
-			((stakePerDay--))
-		fi
+	while (( $stakePerDay >50 && $stakePerDay < 150 ))
+	do
+		flip=$(( RANDOM % 2 ))
+			if [ $flip -eq 1 ]
+			then
+				((stakePerDay++))
+			else
+				((stakePerDay--))
+			fi
+	done
+	total=$(( $total + $stakePerDay))
+	(( daysPlayed++ ))
 done
-echo "You are left with \$$stakePerDay"
+
+echo "Total amount bet in the 20 days is \$$total"
+if [ $total -ge 2000 ]
+then
+	echo "You are in profit in last 20 days "
+	echo "Total profit is \$$(( $total - 2000 ))"
+else
+	echo "You are in loss in last 20 days "
+	echo "Total loss is \$$(( 2000 - $total ))"
+fi

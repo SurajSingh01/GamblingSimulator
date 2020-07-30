079179d8b137a7b58cd23bde39cfb0598a007c7a
SurajSingh01 <singhsurajd03@gmail.com>
2020-07-30 15:20:45 +0530

    Added Loosing or winning a bet for one game

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -7,3 +7,14 @@ stakePerDay=100
 betPerGame=1
 echo "Stakes per day would be \$$stakePerDay"
 echo "Bet to be placed per game will be \$$betPerGame"
+
+flip=$(( RANDOM % 2 ))
+if [ $flip -eq 1 ]
+then
+	echo "You won the bet"
+	((stakePerDay++))
+else
+	echo "You lost the bet"
+	((stakePerDay--))
+fi
+echo "You are left with \$$stakePerDay"
